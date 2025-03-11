7cd9e1c102a11236a1b4b5ada66c3919ca741761
GOPINATH-21BMA023 <gopi63669@gmail.com>
2025-03-11 13:53:52 +0530

    import carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Carousel from './carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
+      <Carousel/>
     </div>
   );
 }
